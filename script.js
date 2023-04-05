80ea444a73d184b7a4323cd9f40335eeebdbe047
zeljko1999 <zeljkoculibrk7@gmail.com>
2023-04-05 16:24:50 +0200

    Add basic math operations to calculator logic

--- script.js
+++ script.js
@@ -25,11 +25,46 @@ function operate(a,operation,b){
         return divide(a,b);
 }
 
-let operantA =0 ;
-let operantB = 0;
-let operator;
+const inputButtons = document.querySelectorAll(".input-button");
+const operatorButtons = document.querySelectorAll(".operation-button");
+const equalsButton = document.querySelector(".equals-button");
+const display = document.querySelector(".display");
+let displayValue = 0;
+let operant1=0;
+let operant2=0;
+let operation = "";
+inputButtons.forEach((inputButton) => {
+    inputButton.addEventListener("click", function(){
+        if(display.textContent == "0")
+            display.textContent = inputButton.textContent;
+        else
+            display.textContent += inputButton.textContent;
+        displayValue = Number(display.textContent);
+    })
+})
 
-console.log(operate(3,"*",4));
-console.log(operate(3,"-",4));
-console.log(operate(3,"+",4));
-console.log(operate(3,"/",4));
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener("click", function(){
+        if(operant1 == 0)
+            {
+            operant1 = displayValue;
+            }
+            else if(operant1 != 0)
+            {
+            operant1 = operate(operant1,operation, displayValue);
+            }
+        operation = operatorButton.textContent;
+        display.textContent = 0;
+        console.log(operant1 + " " + operation);
+            
+    })
+})
+
+equalsButton.addEventListener("click", function() {
+    operant2 = displayValue;
+    displayValue = operate(operant1, operation, operant2);
+    display.textContent = displayValue;
+    operant1 = 0;
+    operant2 = 0;
+    operation = "";
+})
